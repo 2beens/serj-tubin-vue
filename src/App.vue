32cd19f3e2f464f2074c40a7a7993969bd4e5b0e
serj <srdjan@adjust.com>
2020-05-10 11:55:19 +0200

    footer added; bootstrap added

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,20 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/util">Util</router-link>
     </div>
-    <router-view/>
+
+    <router-view />
+
+    <footer class="s-footer fixed-bottom">
+      <div class="row">
+        <div class="col-sm-2">Srđan Tubin</div>
+        <div class="col-sm-1">
+          <a href="https://github.com/2beens" class="fa fa-github"></a>
+        </div>
+        <div class="col-sm-1">
+          <a href="https://www.facebook.com/srdjantubin" class="fa fa-facebook"></a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
