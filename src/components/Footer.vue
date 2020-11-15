e2d7e4e2afb631a35fde09ee1b4c59b1534a1cf8
serj <srdjan@adjust.com>
2020-11-15 19:33:17 +0100

    use new Hetzner API server; version info

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,37 @@
 <template>
   <footer id="s-footer" class="fixed-bottom">
     <div class="row">
-      <div class="col-sm-2">Srđan Tubin</div>
+      <div class="col-sm-1">Srđan Tubin</div>
       <div class="col-sm-1">
         <a href="https://github.com/2beens" class="fa fa-github"></a>
       </div>
-      <div class="col-sm-1">
+      <div class="col-sm-2">
         <a href="https://www.facebook.com/srdjantubin" class="fa fa-facebook"></a>
       </div>
+      <div id="version-info" class="col-sm-1" style="margin-left: 77%; margin-right: 15;">[]</div>
     </div>
   </footer>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  mounted: function () {
+    axios
+      .get(process.env.VUE_APP_API_ENDPOINT + '/version')
+      .then(response => {
+        if (response === null || response.data === null) {
+          console.error('get version info: received null response / data')
+          return
+        }
+        document.getElementById('version-info').innerHTML = '[' + response.data.trim() + ']'
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 }
 </script>
 
