0b9427dea6f99abe7b755e95e71315450c7d289d
serj <stubin87@gmail.com>
2021-10-25 20:45:17 +0200

    yarn build output dir set

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  outputDir: '/var/www/serj-tubin-vue',
   chainWebpack: config => {
     config
       .plugin('html')
       .tap(args => {
-        args[0].title = 'Srđan Tubin'
+        args[0].title = 'Srđan Tubin [Personal Tech Sandbox and Utility]'
         return args
       })
   }
