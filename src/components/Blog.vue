8f99f57301507e9a77d6e0adc8db886be8e67780
serj <stubin87@gmail.com>
2022-10-16 13:56:27 +0200

    blog clap icon update 2

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -110,8 +110,8 @@
             <v-col cols="2" style="padding: 0px">
               <v-btn icon @click="clap(post)">
                 <!-- TODO: not showing in production -->
-                <v-icon>mdi-hand-clap</v-icon>
-                <!-- <v-icon>👏</v-icon> -->
+                <!-- <v-icon>mdi-hand-clap</v-icon> -->
+                <v-icon>👏</v-icon>
                 <!-- <v-icon>mdi-thumb-up</v-icon> -->
               </v-btn>
               {{ post.claps }}
