05560fc229e82aae1f2b8e2747e20fc7dc96e9c4
Serj Tubin <serj.tubin@sumup.com>
2023-04-02 10:37:04 +0200

    sumup login v0.21

--- src/views/SumUp.vue
+++ src/views/SumUp.vue
@@ -1,32 +1,48 @@
 <template>
-  <v-container>
+  <v-container id="main">
     <h2>📝 SumUp Testing / Experimenting / Learning 📝</h2>
 
-    <v-row justify="center" style="margin-top: 20px;">
-
-      <v-col cols="auto">
-        <v-btn @click="onLoginClick" height="72" min-width="164">
-          Login with SumUp
-        </v-btn>
+    <v-row justify="center" style="margin-top: 20px">
+      <v-col v-if="user" cols="auto">
+        <v-btn @click="onLogoutClick" height="72" min-width="164"> Logout from SumUp </v-btn>
+      </v-col>
+      <v-col v-else cols="auto">
+        <v-btn @click="onLoginClick" height="72" min-width="164"> Login with SumUp </v-btn>
       </v-col>
     </v-row>
 
+    <v-row v-if="user" justify="center">
+      <v-col>
+        <h3>Logged in as: {{ user.first_name }} {{ user.last_name }}</h3>
+        <h4>Phone: {{ user.mobile_phone }}</h4>
+        <h4>Location: {{ user.address.city }}, {{ user.address.country }}</h4>
+      </v-col>
+    </v-row>
     <v-row justify="center">
       <v-divider :thickness="6" color="white"></v-divider>
     </v-row>
-    <v-row v-if="user" justify="center">
-      <h3>Logged in as: {{ user.first_name }} {{ user.last_name }}</h3>
-    </v-row>
 
+    <v-row>
+      <h3>Notes:</h3>
+    </v-row>
+    <v-row>
+      <ul>
+        <li>How can we see the list of oauth apps we gave permissions to?</li>
+      </ul>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 import axios from 'axios'
-import { createCodeVerifierAndChallenge, buildAuthorizationUrl, exchangeCodeForToken } from '../oauth'
-const clientId = process.env.VUE_APP_SUMUP_CLIENT_ID;
-const redirectUri = process.env.VUE_APP_SUMUP_REDIRECT_URI;
-const clientSecret = process.env.VUE_APP_SUMUP_CLIENT_SECRET;
+import {
+  createCodeVerifierAndChallenge,
+  buildAuthorizationUrl,
+  exchangeCodeForToken
+} from '../oauth'
+const clientId = process.env.VUE_APP_SUMUP_CLIENT_ID
+const redirectUri = process.env.VUE_APP_SUMUP_REDIRECT_URI
+const clientSecret = process.env.VUE_APP_SUMUP_CLIENT_SECRET
 
 export default {
   name: 'SumUp',
@@ -45,41 +61,50 @@ export default {
       return
     }
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
+    const urlParams = new URLSearchParams(window.location.search)
+    const code = urlParams.get('code')
     if (!code) {
-      return;
+      return
     }
 
     const verifier = this.getCookie('verifier')
-    console.log('found code >', code);
-    console.log('found verifier >', verifier);
+    console.log('found code >', code)
+    console.log('found verifier >', verifier)
 
     const vm = this
     exchangeCodeForToken(clientId, clientSecret, redirectUri, code, verifier)
-      .then(tokenData => {
+      .then((tokenData) => {
         console.log('received token data', tokenData)
         vm.setCookie('access_token', tokenData.access_token, 7)
         vm.setCookie('refresh_token', tokenData.refresh_token, 7)
+        vm.eraseCookie('verifier')
 
-        // TODO: load user
-
+        // remove the search part from the URL
+        const newUrl =
+          window.location.protocol + '//' + window.location.host + window.location.pathname
+        // replace the displayed URL in the address bar
+        window.history.replaceState({}, document.title, newUrl)
+        window.location.reload()
       })
-      .catch(error => {
-        console.error(error);
-      });
+      .catch((error) => {
+        console.error(error)
+      })
   },
 
   methods: {
     async onLoginClick() {
-      const { verifier, challenge } = await createCodeVerifierAndChallenge();
+      const { verifier, challenge } = await createCodeVerifierAndChallenge()
       this.setCookie('verifier', verifier, 1)
 
       // TODO: scope is ignored atm
-      const scope = 'payments user.app-settings transactions.history user.profile_readonly';
-      const authUrl = buildAuthorizationUrl(clientId, redirectUri, challenge, scope);
-      console.warn(authUrl.toString())
-      window.location.href = authUrl.toString();
+      const scope = 'payments user.app-settings transactions.history user.profile_readonly'
+      const authUrl = buildAuthorizationUrl(clientId, redirectUri, challenge, scope)
+      window.location.href = authUrl.toString()
+    },
+    onLogoutClick() {
+      this.eraseCookie('access_token')
+      this.eraseCookie('refresh_token')
+      location.reload()
     },
     async makeApiRequest(url) {
       const accessToken = this.getCookie('access_token')
@@ -89,16 +114,23 @@ export default {
       }
 
       const response = await axios.get(url, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      });
-      return response.data;
+        headers: { Authorization: `Bearer ${accessToken}` }
+      })
+      return response.data
     },
     async getUserData() {
-      // curl -L -X GET 'https://api.sumup.com/v0.1/me/personal-profile' \
-      // -H 'Accept: application/json' \
-      // -H 'Authorization: Bearer <TOKEN>'
       return await this.makeApiRequest('https://api.sumup.com/v0.1/me/personal-profile')
     }
   }
 }
 </script>
+
+<style scoped>
+#main {
+  margin-top: 30px;
+  border-radius: 10px;
+  color: white;
+  padding: 50px;
+  background-color: cadetblue;
+}
+</style>
