7525144facc77e46c7b5ff4a8224ceecafa4a3b3
Serj Tubin <serj.tubin@sumup.com>
2023-04-13 19:32:24 +0200

    cypress login and logout tests

---
+++ cypress/e2e/home-page/loginLogout.cy.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+describe('Ability to log in from the home page', () => {
+  beforeEach(() => {
+    cy.apiMocksSetup()
+    cy.visit('/')
+  })
+
+  it('displays login form', () => {
+    cy.get('#login-button').should('exist').and('be.visible')
+    cy.get('#login-button').click()
+    cy.get('#login-form').should('exist').and('be.visible')
+    cy.get('#login-form-username').should('exist').and('be.visible')
+    cy.get('#login-form-password').should('exist').and('be.visible')
+    cy.get('#login-button').should('exist')
+    cy.contains('button', 'Abort')
+    cy.contains('button', 'Login!')
+
+    // abort first time
+    cy.contains('button', 'Abort').click()
+    cy.get('#login-form-username').should('exist').and('not.be.visible')
+    cy.get('#login-form-password').should('exist').and('not.be.visible')
+
+    cy.get('#login-button').click()
+    // nothing happens as the form is empty
+    cy.get('#login-form-login-button').click()
+    cy.get('#login-form').should('exist').and('be.visible')
+    // set username only, and try clicking the login button
+    cy.get('#login-form-username').type('user1')
+    cy.get('#login-form').should('exist').and('be.visible')
+
+    cy.get('#login-form-password').type('password1')
+    cy.get('#login-form-login-button').should('exist').and('be.visible')
+  })
+
+  it('logs in and out properly', () => {
+    const sessionToken = '1234567890'
+    const loginResponse = `{"token": "${sessionToken}"}`
+    const logoutResponse = 'logged-out'
+    cy.intercept('POST', `${Cypress.config('apiEndpoint')}/a/login`, loginResponse).as('loginApiCall')
+    cy.intercept('GET', `${Cypress.config('apiEndpoint')}/a/logout`, logoutResponse).as('logoutApiCall')
+
+    cy.getCookie('sessionkolacic').should('not.exist')
+
+    cy.get('#login-button').click()
+    cy.get('#login-form-username').type('user1')
+    cy.get('#login-form-password').type('password1')
+
+    cy.getCookie('sessionkolacic').should('not.exist')
+    cy.contains('button', 'Login!').click()
+
+    // check the session cookie was added: sessionkolacic
+    cy.getCookie('sessionkolacic').should('exist')
+    cy.getCookie('sessionkolacic').then((cookie) => {
+      expect(cookie.value).to.be.eq(sessionToken)
+    })
+
+    cy.get('#login-button').should('not.exist')
+    cy.get('#logout-button').should('exist').and('be.visible')
+
+    // try and assert logout now //////////
+    cy.get('#logout-button').click()
+    cy.get('#login-button').should('exist').and('be.visible')
+    cy.get('#logout-button').should('not.exist')
+    cy.getCookie('sessionkolacic').then((cookie) => {
+      expect(cookie).to.be.null
+    })
+  })
+})
--- src/components/LoginLogoutButton.vue
+++ src/components/LoginLogoutButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-btn class="mt-1" id="logout-button" icon v-if="this.$root.loggedIn" @click="logout" color="teal lighten-1">
+    <v-btn class="mt-1" id="logout-button" icon v-if="this.$root.loggedIn" @click="onLogoutClick" color="teal lighten-1">
       <v-icon>mdi-logout</v-icon>
     </v-btn>
 
@@ -10,7 +10,7 @@
           <v-icon>mdi-login</v-icon>
         </v-btn>
       </template>
-      <v-card>
+      <v-card id="login-form">
         <v-card-title>
           <span class="headline">Login</span>
         </v-card-title>
@@ -19,6 +19,7 @@
             <v-row>
               <v-col cols="12">
                 <v-text-field
+                  id="login-form-username"
                   label="Username"
                   required
                   autofocus
@@ -27,6 +28,7 @@
               </v-col>
               <v-col cols="12">
                 <v-text-field
+                  id="login-form-password"
                   label="Password"
                   required
                   v-model="user.password"
@@ -43,7 +45,7 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" text @click="showLoginDialog = false"> Abort </v-btn>
-          <v-btn color="blue darken-1" text @click="login"> Login! </v-btn>
+          <v-btn color="blue darken-1" text @click="onLoginClick" id="login-form-login-button"> Login! </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -80,13 +82,13 @@ export default {
         this.login()
       }
     },
-    login: function () {
+    onLoginClick: function () {
       if (this.user.name === undefined || this.user.name === '') {
-        console.error('emtpy username')
+        console.error('empty username')
         return
       }
       if (this.user.password === undefined || this.user.password === '') {
-        console.error('emtpy password')
+        console.error('empty password')
         return
       }
 
@@ -120,7 +122,7 @@ export default {
           vm.showLoginDialog = false
         })
     },
-    logout: function () {
+    onLogoutClick: function () {
       const vm = this
       axios
         .get(process.env.VUE_APP_API_ENDPOINT + '/a/logout', {
