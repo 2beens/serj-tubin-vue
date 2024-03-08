c9490970d7582fa345be603b60a5608293c3f043
Serj Tubin <stubin87@gmail.com>
2024-03-08 13:43:06 +0100

    gymstats skeleton fix

--- src/components/gymstats/ExercisesList.vue
+++ src/components/gymstats/ExercisesList.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container>
     <v-row v-if="loadingData">
-      <v-skeleton-loader type="sentences"></v-skeleton-loader>
-      <v-skeleton-loader type="table-heading"></v-skeleton-loader>
-      <v-skeleton-loader type="table-tbody"></v-skeleton-loader>
+      <v-col>
+        <v-skeleton-loader type="sentences"></v-skeleton-loader>
+        <v-skeleton-loader type="table-heading"></v-skeleton-loader>
+        <v-skeleton-loader type="table-tbody"></v-skeleton-loader>
+      </v-col>
     </v-row>
 
     <v-row v-if="$vuetify.breakpoint.mdAndUp">
