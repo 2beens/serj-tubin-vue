0030218daffc036dd61e545d234644e44b975a16
Serj Tubin <stubin87@gmail.com>
2024-03-17 16:38:58 +0100

    exercise timeline component

--- src/components/gymstats/ExercisesList.vue
+++ src/components/gymstats/ExercisesList.vue
@@ -152,49 +152,22 @@
 
     <v-row v-if="loadedExerciseHistory" ref="loadedExerciseHistory">
       <v-col>
-        <v-timeline align-top dense dark>
-          <v-timeline-item v-for="(stats, date) in loadedExerciseHistory.stats" :key="date" small>
-            <v-card dark class="mx-auto pa-0" max-width="600">
-              <v-card-title>{{ date }}</v-card-title>
-              <v-card-text>
-                <v-row>
-                  <v-col>
-                    <v-chip color="teal lighten-1" label>
-                      <v-icon left> mdi-weight-lifter </v-icon>
-                      Avg. Kilos
-                    </v-chip>
-                    <v-chip class="ml-2" color="primary">{{ stats.avgKilos }}</v-chip>
-                  </v-col>
-                  <v-col>
-                    <v-chip color="teal lighten-1" label>
-                      <v-icon left> mdi-dumbbell </v-icon>
-                      Avg. Reps
-                    </v-chip>
-                    <v-chip class="ml-2" color="primary">{{ stats.avgReps }}</v-chip>
-                  </v-col>
-                  <v-col>
-                    <v-chip color="teal lighten-1" label>
-                      <v-icon left> mdi-clock-time-four-outline </v-icon>
-                      Sets
-                    </v-chip>
-                    <v-chip class="ml-2" color="primary">{{ stats.sets }}</v-chip>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-card>
-          </v-timeline-item>
-        </v-timeline>
+        <ExercisesTimeline :loadedExerciseStats="loadedExerciseHistory.stats" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<script>
+<script scoped>
 import axios from 'axios'
 import GymStatsData from '@/gymstats'
+import ExercisesTimeline from '@/components/gymstats/ExerciseTimeline.vue'
 
 export default {
   name: 'ExerciseList',
+  components: {
+    ExercisesTimeline
+  },
 
   data: function () {
     return {
